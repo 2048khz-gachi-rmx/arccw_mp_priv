5ffee5ebdcf21eb136f2319da1736a8d776c7edc
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-04-09 14:09:25 +0700

    unfuck some high-magnification scopes

--- lua/arccw_all_atts/arccw_atts_8.lua
+++ lua/arccw_all_atts/arccw_atts_8.lua
@@ -166,66 +166,6 @@ do
 end
 
 ------
--- optic_cheytacscope.lua
-------
-
-do
-	local att = {}
-	ArcCW.SetShortName("optic_cheytacscope")
-
-	att.PrintName = "CheyTac (2-4.3x)"
-	att.Icon = Material("entities/acwatt_optic_cheytacscope.png", "smooth")
-	att.Description = "Good scope. Gooood scope."
-
-	att.SortOrder = 4.3
-
-	att.Desc_Pros = {
-	    "autostat.holosight",
-	    "autostat.zoom",
-	}
-	att.Desc_Cons = {
-	}
-	att.AutoStats = true
-	att.Slot = {"optic","optic_sniper"}
-
-	att.Model 			= "models/weapons/arccw/fesiugmw2/atts/cheytacscope_3b.mdl"
-	att.HolosightPiece 	= "models/weapons/arccw/fesiugmw2/atts/cheytacscope_2b_hsp.mdl"
-
-	att.AdditionalSights = {
-	    {
-	        Pos = Vector(0, 12, -1.5),
-	        Ang = Angle(0, 0, 0),
-	        ViewModelFOV = 65 / 2.25,
-	        Magnification = 2.25,
-	        ScrollFunc = ArcCW.SCROLL_ZOOM,
-	        ZoomLevels = 6,
-	        ZoomSound = "weapons/arccw/fiveseven/fiveseven_slideback.wav",
-	        IgnoreExtra = true
-	    }
-	}
-
-	att.ScopeGlint = true
-
-	att.Holosight = true
-	att.HolosightReticle = Material("hud/scopes/m40a3.png", "mips smooth")
-	att.HolosightNoFlare = true
-	att.HolosightSize = 25
-	att.HolosightBone = "holosight"
-	att.Colorable = false
-
-	att.HolosightMagnification = 4.3
-	att.HolosightBlackbox = true
-
-	att.HolosightMagnificationMin = 2
-	att.HolosightMagnificationMax = 4.3
-
-	att.Mult_SightTime = 1.15
-	att.Mult_SightedSpeedMult = 0.9
-	att.Mult_SpeedMult = 0.98
-	ArcCW.LoadAttachmentType(att)
-end
-
-------
 -- optic_delta.lua
 ------
 
@@ -448,7 +388,7 @@ do
 
 	att.AdditionalSights = {
 	    {
-	        Pos = Vector(0, 20, -1.525),
+	        Pos = Vector(0, 10, -1.525),
 	        Ang = Angle(0, 0, 0),
 	        Magnification = 2,
 	        ScopeMagnification = 2.7,
@@ -665,68 +605,6 @@ do
 	att.Mult_SightTime = 1.05
 
 	att.Colorable = true
-	ArcCW.LoadAttachmentType(att)
-end
-
-------
--- optic_m82scope.lua
-------
-
-do
-	local att = {}
-	ArcCW.SetShortName("optic_m82scope")
-
-	att.PrintName = "Barrett (2-4.3x)"
-	att.Icon = Material("entities/acwatt_optic_m82scope.png", "smooth")
-	att.Description = "Good scope. Gooood scope."
-
-	att.SortOrder = 4.3
-
-	att.Desc_Pros = {
-	    "autostat.holosight",
-	    "autostat.zoom",
-	}
-	att.Desc_Cons = {
-	}
-	att.AutoStats = true
-	att.Slot = {"optic","optic_sniper"}
-
-	att.Model 			= "models/weapons/arccw/fesiugmw2/atts/m82_4.mdl"
-	att.HolosightPiece 	= "models/weapons/arccw/fesiugmw2/atts/m82_3_hsp.mdl"
-
-	att.AdditionalSights = {
-	    {
-	        Pos = Vector(0.0231, 11, -1.01),
-	        Ang = Angle(0, 0, 0),
-	        ViewModelFOV = 65 / 2.25,
-	        Magnification = 2.25,
-	        ScrollFunc = ArcCW.SCROLL_ZOOM,
-	        ZoomLevels = 6,
-	        ZoomSound = "weapons/arccw/fiveseven/fiveseven_slideback.wav",
-	        IgnoreExtra = true
-	    }
-	}
-
-	att.ScopeGlint = true
-
-	att.Holosight = true
-	att.HolosightReticle = Material("hud/scopes/m40a3.png", "mips smooth")
-	att.HolosightNoFlare = true
-	att.HolosightSize = 25
-	att.HolosightBone = "holosight"
-	att.Colorable = false
-
-	att.HolosightMagnification = 4.3
-	att.HolosightBlackbox = true
-
-	att.HolosightMagnificationMin = 2
-	att.HolosightMagnificationMax = 4.3
-
-	att.Mult_SightTime = 1.15
-	att.Mult_SightedSpeedMult = 0.9
-	att.Mult_SpeedMult = 0.98
-
-	att.ModelOffset = Vector(-2.6, 0, -0.55)
 	ArcCW.LoadAttachmentType(att)
 end
 
