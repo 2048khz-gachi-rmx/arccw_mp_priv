fd207c4ab004cc1afc76b0eb83e7e342667e61e1
aeee <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2022-04-18 18:48:26 +0700

    -mw atts

---
+++ lua/arccw_all_atts/arccw_mw_atts_1.lua
@@ -0,0 +1,90 @@
+local Material = ArcCW.AttachmentMat
+
+------
+-- holger26_dream_maker.lua
+------
+
+do
+	local att = {}
+	ArcCW.SetShortName("holger26_dream_maker")
+
+	att.PrintName = "Dream Maker"
+	att.Icon = Material("entities/att/acwatt_holger26_dream_maker.png")
+	att.Description = "Holger-26 Dream Maker."
+	att.Desc_Pros = {
+	    "- Cosmetic skin",
+	}
+	att.Desc_Cons = {
+	}
+	att.Slot = "holger26_dream_maker"
+	att.Free = true
+	att.ActivateElements = {"dream_maker"}
+	ArcCW.LoadAttachmentType(att)
+end
+
+------
+-- kimber_pinecone.lua
+------
+
+do
+	local att = {}
+	ArcCW.SetShortName("kimber_pinecone")
+
+	att.PrintName = "Pinecone"
+	att.Icon = Material("entities/att/acwatt_m1911_pinecone.png")
+	att.Description = "M1911 Pinecone."
+	att.Desc_Pros = {
+	    "- Cosmetic skin",
+	}
+	att.Desc_Cons = {
+	}
+	att.Slot = "m1911_pinecone"
+	att.Free = true
+	att.ActivateElements = {"pinecone"}
+	ArcCW.LoadAttachmentType(att)
+end
+
+------
+-- oden_dream_killer.lua
+------
+
+do
+	local att = {}
+	ArcCW.SetShortName("oden_dream_killer")
+
+	att.PrintName = "Dream Killer"
+	att.Icon = Material("entities/att/acwatt_oden_dream_killer.png")
+	att.Description = "Oden Dream Killer."
+	att.Desc_Pros = {
+	    "- Cosmetic skin",
+	}
+	att.Desc_Cons = {
+	}
+	att.Slot = "oden_dream_killer"
+	att.Free = true
+	att.ActivateElements = {"dream_killer"}
+	ArcCW.LoadAttachmentType(att)
+end
+
+------
+-- rytec_amr_showcase.lua
+------
+
+do
+	local att = {}
+	ArcCW.SetShortName("rytec_amr_showcase")
+
+	att.PrintName = "Showcase"
+	att.Icon = Material("entities/att/acwatt_rytec_amr_showcase.png")
+	att.Description = "Rytec AMR Showcase."
+	att.Desc_Pros = {
+	    "- Cosmetic skin",
+	}
+	att.Desc_Cons = {
+	}
+	att.Slot = "rytec_amr_showcase"
+	att.Free = true
+	att.ActivateElements = {"showcase"}
+	ArcCW.LoadAttachmentType(att)
+end
+
